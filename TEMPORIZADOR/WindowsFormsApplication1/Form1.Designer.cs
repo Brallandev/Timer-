a5356ce9503f46a0f00475c177d94595af7ec3d7
Brallandev <brallandevelop@gmail.com>
2017-04-20 18:45:11 -0500

    MAIN CODE
    codigo fuente del proyecto/main code of project

---
+++ TEMPORIZADOR/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,314 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button4 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.lbl3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(161, 43);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            60,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(49, 20);
+            this.numericUpDown1.TabIndex = 0;
+            this.numericUpDown1.ThousandsSeparator = true;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.numericUpDown2.Location = new System.Drawing.Point(542, 43);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(49, 20);
+            this.numericUpDown2.TabIndex = 1;
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.numericUpDown3.Location = new System.Drawing.Point(919, 43);
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(49, 20);
+            this.numericUpDown3.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(93, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "horas";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(468, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Minutos";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(851, 43);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Segundos";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(306, 99);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Guardar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lbl1
+            // 
+            this.lbl1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.lbl1.AutoSize = true;
+            this.lbl1.Font = new System.Drawing.Font("Consolas", 140.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl1.Location = new System.Drawing.Point(83, 269);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(298, 219);
+            this.lbl1.TabIndex = 7;
+            this.lbl1.Text = "00";
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(306, 99);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Corregir";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(661, 99);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Iniciar";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button4
+            // 
+            this.button4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.button4.Location = new System.Drawing.Point(661, 99);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "Parar";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Visible = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label5
+            // 
+            this.label5.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(151, 214);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(145, 55);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "horas";
+            // 
+            // label6
+            // 
+            this.label6.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(461, 214);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(192, 55);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Minutos";
+            // 
+            // label7
+            // 
+            this.label7.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(726, 214);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(242, 55);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Segundos";
+            // 
+            // lbl2
+            // 
+            this.lbl2.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.lbl2.AutoSize = true;
+            this.lbl2.Font = new System.Drawing.Font("Consolas", 140.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl2.Location = new System.Drawing.Point(402, 269);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(298, 219);
+            this.lbl2.TabIndex = 14;
+            this.lbl2.Text = "00";
+            // 
+            // lbl3
+            // 
+            this.lbl3.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.lbl3.AutoSize = true;
+            this.lbl3.Font = new System.Drawing.Font("Consolas", 140.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl3.Location = new System.Drawing.Point(699, 269);
+            this.lbl3.Name = "lbl3";
+            this.lbl3.Size = new System.Drawing.Size(298, 219);
+            this.lbl3.TabIndex = 15;
+            this.lbl3.Text = "00";
+            // 
+            // label4
+            // 
+            this.label4.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Consolas", 140.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(331, 269);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(99, 219);
+            this.label4.TabIndex = 16;
+            this.label4.Text = ":";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label8
+            // 
+            this.label8.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.label8.BackColor = System.Drawing.Color.Transparent;
+            this.label8.Font = new System.Drawing.Font("Consolas", 140.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(646, 269);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(99, 219);
+            this.label8.TabIndex = 17;
+            this.label8.Text = ":";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1112, 572);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lbl3);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Temporiador";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.Label lbl3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ TEMPORIZADOR/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Media;
+
+namespace WindowsFormsApplication1
+{   
+    public partial class Form1 : Form
+    {
+        int h;
+        int m;
+        int s;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            h = Convert.ToInt32(numericUpDown1.Value);
+            m = Convert.ToInt32(numericUpDown2.Value);
+            s = Convert.ToInt32(numericUpDown3.Value);
+
+
+
+            if (h <= 0 || h > 180 || m <= 0 || m > 59 || s <= 0 || s > 59)
+            {
+                MessageBox.Show("Valores no validos", "No se pudo guardar", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            else
+            {
+
+
+
+                if (h < 10 & m < 10 & s < 10)
+                {
+                    lbl1.Text = ("0" + h);
+                    lbl2.Text = ("0" + m);
+                    lbl3.Text = ("0" + s);
+                }
+
+                if (m < 10 & s < 10)
+                {
+                    lbl1.Text = ("" + h);
+                    lbl2.Text = ("0" + m);
+                    lbl3.Text = ("0" + s);
+
+                }
+                if (m > 10 & s < 10)
+                {
+
+                    lbl1.Text = ("" + h);
+                    lbl2.Text = ("" + m);
+                    lbl3.Text = ("0" + s);
+
+                }
+
+
+                if (s >= 10)
+                {
+                    lbl1.Text = ("" + h);
+                    lbl2.Text = ("" + m);
+                    lbl3.Text = ("" + s);
+
+                }
+                if (h < 10)
+                {
+                    lbl1.Text = ("0" + h);
+
+                }
+                if (m < 10)
+                {
+                    lbl2.Text = ("0" + m);
+
+                }
+                numericUpDown1.Enabled = false;
+                numericUpDown2.Enabled = false;
+                numericUpDown3.Enabled = false;
+
+                button2.Visible = true;
+                button3.Enabled = true;
+                button2.Enabled = false;
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button3.Visible = true;
+            button4.Visible = false;
+
+
+            button2.Visible = false;
+
+            button1.Visible = true;
+
+            numericUpDown1.Enabled = true;
+            numericUpDown2.Enabled = true;
+            numericUpDown3.Enabled = true;
+
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (h > 0 & m == 0)
+            {
+                h = h - 1;
+
+                m = 60;
+
+            }
+
+            if (s != 0)
+            {
+
+                s = s - 1;
+
+
+            }
+            else {
+
+                s = 59;
+
+                m = m - 1;
+            }
+
+
+//Impresiones...................................
+
+            if (h < 10 & m < 10 & s < 10)
+            {
+                lbl1.Text = ("0" + h);
+                lbl2.Text = ("0" + m);
+                lbl3.Text = ("0" + s);
+            }
+
+            if (m < 10 & s < 10)
+            {
+                lbl1.Text = ("" + h);
+                lbl2.Text = ("0" + m);
+                lbl3.Text = ("0" + s);
+
+            }
+            if (m > 10 & s < 10)
+            {
+
+                lbl1.Text = ("" + h);
+                lbl2.Text = ("" + m);
+                lbl3.Text = ("0" + s);
+
+            }
+
+
+            if (s > 10)
+            {
+                lbl1.Text = ("" + h);
+                lbl2.Text = ("" + m);
+                lbl3.Text = ("" + s);
+
+            }
+            if (h < 10)
+            {
+                lbl1.Text = ("0" + h);
+
+            }
+            if (m < 10)
+            {
+                lbl2.Text = ("0" + m);
+
+            }
+            if (h == 0 & m < 1) {
+
+                lbl1.ForeColor=System.Drawing.Color.Red;
+                lbl2.ForeColor = System.Drawing.Color.Red;
+                lbl3.ForeColor = System.Drawing.Color.Red;
+
+            }
+
+            if (m > 1)
+            {
+
+                lbl1.ForeColor = System.Drawing.Color.Black;
+                lbl2.ForeColor = System.Drawing.Color.Black;
+                lbl3.ForeColor = System.Drawing.Color.Black;
+
+
+
+            }
+
+            if (h == 0 & m == 0 & s == 0) {
+
+                timer1.Stop();
+
+                
+
+                try
+                {
+
+                    SoundPlayer alarm = new SoundPlayer(WindowsFormsApplication1.Properties.Resources.Free_Converter_com_school_bell_ringing___sound_effect_1_2625627);
+
+                    alarm.Play(); 
+                }
+                catch
+                {
+
+
+                    MessageBox.Show("Un Error Ha Ocurrido", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+
+                }
+
+                MessageBox.Show("Tiempo Finalizado", "Fin Del Tiempo", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+
+                Application.Restart();
+
+            }
+
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled=true;
+
+            button3.Visible = false;
+            button4.Visible = true;
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            button2.Enabled = true;
+        }
+    }
+}
